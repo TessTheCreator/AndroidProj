93730741db72a60b3bbf778e90e5615a6a7a5883
RawanFarakhnah <rawanali1997@yahoo.com>
2020-04-14 01:00:54 +0300

    FirstActivityDesign

---
+++ app/src/main/java/com/example/hotelapp/FindHotel_Activity.java
@@ -0,0 +1,14 @@
+package com.example.hotelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindHotel_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_hotel_);
+    }
+}
